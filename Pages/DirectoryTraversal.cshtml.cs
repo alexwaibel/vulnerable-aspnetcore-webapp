dc608dca6cba2cc8546884e24734d8fc41b79025
Alex Waibel <alexwaibel@users.noreply.github.com>
2023-12-03 17:46:14 -0800

    Initial directory traversal page

--- Pages/DirectoryTraversal.cshtml.cs
+++ Pages/DirectoryTraversal.cshtml.cs
@@ -6,14 +6,20 @@ namespace vulnerableaspnetcoreapp.Pages;
 public class DirectoryTraversalModel : PageModel
 {
     private readonly ILogger<DirectoryTraversalModel> _logger;
+    public string? DangerousFileContent { get; private set; }
 
     public DirectoryTraversalModel(ILogger<DirectoryTraversalModel> logger)
     {
         _logger = logger;
     }
 
-    public void OnGet()
+    public void OnGet(string fileName = "danger.jpg")
     {
+        var fullPath = Path.Combine([System.IO.Directory.GetCurrentDirectory(), "wwwroot", "images", fileName]);
+        byte[] fileBytes = System.IO.File.ReadAllBytes(fullPath);
+        string base64EncodedData = Convert.ToBase64String(fileBytes);
+        string imgDataURL = string.Format("data:image/png;base64,{0}", base64EncodedData);
+        DangerousFileContent = imgDataURL;
     }
 }
 
