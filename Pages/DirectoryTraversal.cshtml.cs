4543640a57c036daa31f02120c72bd2e1e235db8
Alex Waibel <alexwaibel@users.noreply.github.com>
2023-12-03 19:02:55 -0800

    Add more details to traversal examples

--- Pages/DirectoryTraversal.cshtml.cs
+++ Pages/DirectoryTraversal.cshtml.cs
@@ -7,19 +7,26 @@ public class DirectoryTraversalModel : PageModel
 {
     private readonly ILogger<DirectoryTraversalModel> _logger;
     public string? DangerousFileContent { get; private set; }
+    [BindProperty(SupportsGet = true)]
+    public string FileName { get; set; }
 
     public DirectoryTraversalModel(ILogger<DirectoryTraversalModel> logger)
     {
         _logger = logger;
+        FileName = "danger.jpg";
     }
 
-    public void OnGet(string fileName = "danger.jpg")
+    public void OnGet()
     {
-        var fullPath = Path.Combine([System.IO.Directory.GetCurrentDirectory(), "wwwroot", "images", fileName]);
-        byte[] fileBytes = System.IO.File.ReadAllBytes(fullPath);
+        var fullPath = Path.Combine([System.IO.Directory.GetCurrentDirectory(), "wwwroot", "images", FileName]);
+        DangerousFileContent = this.FetchImageDataURLFromPath(fullPath);
+    }
+
+    private string FetchImageDataURLFromPath(string path)
+    {
+        byte[] fileBytes = System.IO.File.ReadAllBytes(path);
         string base64EncodedData = Convert.ToBase64String(fileBytes);
-        string imgDataURL = string.Format("data:image/png;base64,{0}", base64EncodedData);
-        DangerousFileContent = imgDataURL;
+        return string.Format("data:image/png;base64,{0}", base64EncodedData);
     }
 }
 
