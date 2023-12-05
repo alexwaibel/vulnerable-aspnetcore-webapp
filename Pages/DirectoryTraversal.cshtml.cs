9fd20564b9b5434ed4e957481d115846b6580b55
Alex Waibel <alexwaibel@users.noreply.github.com>
2023-12-04 18:08:51 -0800

    Clean up formatting, add mitigations and allowlist example

--- Pages/DirectoryTraversal.cshtml.cs
+++ Pages/DirectoryTraversal.cshtml.cs
@@ -8,18 +8,33 @@ public class DirectoryTraversalModel : PageModel
     private readonly ILogger<DirectoryTraversalModel> _logger;
     public string? DangerousFileContent { get; private set; }
     [BindProperty(SupportsGet = true)]
-    public string FileName { get; set; }
+    public string DangerousFileNameInput { get; set; }
+    [BindProperty(SupportsGet = true)]
+    public string AllowlistedFileNameInput { get; set; }
+    public string? AllowlistedFileContent { get; private set; }
 
     public DirectoryTraversalModel(ILogger<DirectoryTraversalModel> logger)
     {
         _logger = logger;
-        FileName = "danger.jpg";
+        DangerousFileNameInput = "danger.jpg";
+        AllowlistedFileNameInput = "hardhat.jpg";
     }
 
     public void OnGet()
     {
-        var fullPath = Path.Combine([System.IO.Directory.GetCurrentDirectory(), "wwwroot", "images", FileName]);
-        DangerousFileContent = this.FetchImageDataURLFromPath(fullPath);
+        DangerousFileContent = DangerouslyFetchDataURLFromFilenameInput(DangerousFileNameInput);
+        AllowlistedFileContent = FetchAllowlistedDataURLFromPath(AllowlistedFileNameInput);
+    }
+
+    private string DangerouslyFetchDataURLFromFilenameInput(string filename)
+    {
+        return this.FetchImageDataURLFromPath(Path.Combine([System.IO.Directory.GetCurrentDirectory(), "wwwroot", "images", DangerousFileNameInput]));
+    }
+
+    private string FetchAllowlistedDataURLFromPath(string path)
+    {
+        string[] allowedFiles = ["danger.jpg", "hardhat.jpg"];
+        return allowedFiles.Contains(path) ? FetchImageDataURLFromPath(Path.Combine([System.IO.Directory.GetCurrentDirectory(), "wwwroot", "images", path])) : "";
     }
 
     private string FetchImageDataURLFromPath(string path)
